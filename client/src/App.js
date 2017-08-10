8a3ba6432a51edb1892ecc9339416cf426ce497e
burnsbeaver <burnsbeaver@gmail.com>
2017-08-10 17:50:08 -0400

    Imported Client depencies, and created client side user show route

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import User from "./components/User"
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <Router>
+        <div>
+          <h1>LANDING PAGE</h1>
+          <div>
+            <Link to="/">Home</Link>
+            <Link to="/user/:userId">Login</Link>
+          </div>
+          <div>
+            <Route path="/user/:userId" component={User} />
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      </Router>
     );
   }
 }
